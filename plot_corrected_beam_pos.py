ec0042a16d172399a98df610f18d60e63f503c23
VJMReichenbach <Valentin.Reichenbach@live.com>
2023-05-16 15:30:26 +0200

    added ability to show how the pos would have looked without controller

--- plot_corrected_beam_pos.py
+++ plot_corrected_beam_pos.py
@@ -17,21 +17,42 @@ def read_data(file: Path):
             new_current.append(float(lines[i].split(',')[5]))
     return time, current_pos, corrected_pos, current_shift, current_current, new_current
 
+def delta_I_to_delta_x(delta_I: float):
+    # Output of calibration:
+    # x(I) = -45.796722 * I + 410.90
+    # y(I) = -0.210579 * I + 342.08
+    # I(x) = -0.021836 * x + -8.97
+    # I(y) = -4.748816 * y + -1624.48
+    return -45.796722 * delta_I + 410.90
+
 def main(file: Path):
     time, current_pos, corrected_pos, current_shift, current_current, new_current = read_data(file)
 
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
-    ax1.plot(time, current_pos, label='Current pos')
-    ax1.plot(time, corrected_pos, label='Corrected pos')
-    ax1.set_ylabel('Position (px)')
+    not_controlled_pos = []
+
+    # The first position is the initial position and is not controlled
+    first_pos = current_pos[0]
+
+    not_controlled_pos.append(first_pos)
+
+    for i in range(len(current_shift)):
+        # Skip the first one because it's the initial position
+        if i == 0:
+            continue
+        x_shift = delta_I_to_delta_x(current_shift[i])
+        not_controlled_pos.append(not_controlled_pos[i-1] + x_shift)
+
+    # plot the not controlled position and the corrected position
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    ax1.plot(time, not_controlled_pos, label='Not controlled')
+    ax1.plot(time, corrected_pos, label='Corrected')
+    ax1.set_ylabel('x (px)')
     ax1.legend()
-    ax2.plot(time, current_shift)
-    ax2.set_ylabel('Shift (px)')
-    ax3.plot(time, current_current)
-    ax3.set_ylabel('Current (A)')
-    ax4.plot(time, new_current)
-    ax4.set_ylabel('New current (A)')
-    ax4.set_xlabel('Time (ms)')
+    ax2.plot(time, current_current, label='Current')
+    ax2.plot(time, new_current, label='New current')
+    ax2.set_ylabel('Current (A)')
+    ax2.set_xlabel('Time (ms)')
+    ax2.legend()
     plt.show()
 
 if __name__ == '__main__':
